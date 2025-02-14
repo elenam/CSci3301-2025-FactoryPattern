d81882578ed38fc28945a1066a8a38e9a99703bc
Elena M <elenam@morris.umn.edu>
2025-02-14 13:39:47 -0600

    changed enum to string array

--- src/cardgames/RegularCard.java
+++ src/cardgames/RegularCard.java
@@ -1,14 +1,17 @@
 package cardgames;
 
 public class RegularCard implements Card {
-	public static enum Suit {CLUBS, SPADES, HEARTS, DIAMONDS};;
+	// Suits and ranks could be enums, but then they are different types
+	// in different kinds of cards. Making them strings unifies the 
+	// implementation, and also introduces an ordering. 
+	public static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 	public static String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
 			"Jack", "Queen", "King", "Ace"};
-	private Suit suit;
+	private String suit;
 	private String rank;
 	
 	
-	public RegularCard(Suit suit, String rank) {
+	public RegularCard(String suit, String rank) {
 		this.suit = suit;
 		this.rank = rank;
 	}
